ec0fc9b12fadb4ce57c561b3e42264ca29592499
jesse harlan <gotsanity@gmail.com>
2023-02-01 10:14:16 -0800

    Fixed issue with proxy configuration so api calls will now work in the clientapp. Also added a project-list component and data service to test the api in the client.

---
+++ CCDevTools/ClientApp/src/app/data.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  baseUrl: string;
+
+  constructor(private http: HttpClient, @Inject('BASE_URL') base: string) {
+    this.baseUrl = base + "api";
+  }
+
+  getAllProjects(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/projects`);
+  }
+}
