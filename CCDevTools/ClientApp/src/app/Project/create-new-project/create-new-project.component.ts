7217105db6a7cfba26c03218340bd3715b9edc4b
zitanicole <zita.kellogg@student.centralia.edu>
2023-03-08 20:41:19 -0800

    create new project but the submit button doesnt work

--- CCDevTools/ClientApp/src/app/Project/create-new-project/create-new-project.component.ts
+++ CCDevTools/ClientApp/src/app/Project/create-new-project/create-new-project.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataService } from '../../data.service';
 
 @Component({
   selector: 'app-create-new-project',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateNewProjectComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private data: DataService, private router: Router) { }
+  newProjectForm = this.fb.group({
+    id: [0],
+    name: ['Name Text'],
+    description: ['Describing text'],
+    startdate: [Date.now()],
+    version: [0],
+    url: ['url.com']
+  });
 
   ngOnInit(): void {
   }
 
+  onSubmit()
+  {
+    let project =
+    {
+      id: 0,
+      name: this.newProjectForm.value.name,
+      description: this.newProjectForm.value.description,
+      startdate: Date.now,
+      version: this.newProjectForm.value.version,
+      url: this.newProjectForm.value.url
+    };
+
+    console.log('before submit', project)
+
+    this.data.createNewProject(project).subscribe(results => {
+      console.log(results);
+      this.router.navigate(['/projects', results.id]);
+    });
+  }
 }
