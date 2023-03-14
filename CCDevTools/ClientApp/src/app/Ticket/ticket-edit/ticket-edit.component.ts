04b8f9e8075bded682ada9157ef007acfdbf37cc
Broc Keeton <broc.keeton@student.centralia.edu>
2023-03-14 09:20:36 -0700

    Ticket Edit feature added but problem getting modified to return value.

--- CCDevTools/ClientApp/src/app/Ticket/ticket-edit/ticket-edit.component.ts
+++ CCDevTools/ClientApp/src/app/Ticket/ticket-edit/ticket-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { DataService } from '../../data.service';
 
 @Component({
   selector: 'app-ticket-edit',
@@ -7,9 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketEditComponent implements OnInit {
 
-  constructor() { }
+  id: number = 0;
+
+  ticket = {};
+
+  ticketForm = this.fb.group({
+    id: [0],
+    description: [''],
+    status: ["0"],
+    projectId: ["1"],
+    created: [Date.now()],
+    modified: [Date.now()]
+  });
+
+  constructor(private data: DataService, private fb: FormBuilder, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.route.paramMap.pipe(
+      switchMap(params => {
+        this.id = Number(params.get('id'));
+        return this.data.getTicketById(this.id);
+      })
+    ).subscribe(ticketData => {
+      console.log(ticketData);
+      this.ticket = ticketData;
+
+      this.ticketForm.patchValue({
+        id: this.id,
+        description: ticketData.description,
+        status: ticketData.status,
+        projectId: ticketData.projectId,
+        modified: ticketData.modified
+      });
+    });
+  }
+
+  onSubmit(): void {
+    let ticket = {
+      id: this.ticketForm.value.id,
+      description: this.ticketForm.value.description,
+      status: this.ticketForm.value.status,
+      projectId: this.ticketForm.value.projectId,
+      created: Date.now,
+      modified: Date.now
+    };
+
+    console.log('before submit', ticket);
+
+    this.data.updateTicket(ticket).subscribe(results => {
+      console.log(results);
+      this.router.navigate(['/tickets', this.id]);
+    });
   }
 
 }
