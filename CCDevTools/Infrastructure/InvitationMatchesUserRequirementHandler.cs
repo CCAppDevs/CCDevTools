cd1de57591aca79fdca277967a33c7a958da9201
Rayleen Roberts <rayleen.roberts@student.centralia.edu>
2023-05-30 12:53:37 -0700

    Checking that member id matches invitation id OR if the user is the invitation owner in  order to limit deletion priviledges

--- CCDevTools/Infrastructure/InvitationMatchesUserRequirementHandler.cs
+++ CCDevTools/Infrastructure/InvitationMatchesUserRequirementHandler.cs
@@ -1,6 +1,50 @@
-﻿namespace CCDevTools.Infrastructure
+﻿using CCDevTools.Data;
+using CCDevTools.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace CCDevTools.Infrastructure
 {
-    public class InvitationMatchesUserRequirementHandler
+    public class InvitationMatchesUserRequirementHandler : AuthorizationHandler<UserMatchesRequirement>
     {
+
+        private readonly ApplicationDbContext _context;
+
+        public InvitationMatchesUserRequirementHandler(ApplicationDbContext ctx)
+        {
+            _context = ctx;
+        }
+
+        protected override  Task HandleRequirementAsync(AuthorizationHandlerContext authContext, UserMatchesRequirement requirement)
+        {
+            var invitation = authContext.Resource as Invitation;
+
+            if (invitation == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            var userID = authContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (userID == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            var invitedUser = _context.Users.Where(u => u.Email == invitation.Email).FirstOrDefault();
+
+            if (invitedUser == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            if (invitedUser.Id == userID)
+            {
+                authContext.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+
     }
 }
--- CCDevTools/Infrastructure/UserClaimsTransformation.cs
+++ CCDevTools/Infrastructure/UserClaimsTransformation.cs
@@ -1,6 +1,25 @@
-﻿namespace CCDevTools.Infrastructure
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
+namespace CCDevTools.Infrastructure
 {
-    public class UserClaimsTransformation
+    public class UserClaimsTransformation : IClaimsTransformation
     {
+        public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+        {
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity();
+            var claimType = "email";
+
+            
+
+            if (!principal.HasClaim(claim => claim.Type == claimType))
+            {
+                claimsIdentity.AddClaim(new Claim(claimType, "email"));
+            }
+            
+            principal.AddIdentity(claimsIdentity);
+            
+            return Task.FromResult(principal);
+        }
     }
 }
